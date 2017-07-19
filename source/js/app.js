9a229acc8bf8cb1ca0f948c38e8cafddb2863ce9
Dmytro Golovko <dmtrglvk@gmail.com>
2017-07-19 22:42:31 +0300

    login page, svg refactoring

--- source/js/app.js
+++ source/js/app.js
@@ -1,1 +1,26 @@
 var $ = require('jquery');
+
+$(function(){
+	forgotLogin();
+});
+
+
+function forgotLogin() {
+	var forgotLink = $('.js-forgot-password'),
+		loginContent = $('.js-login-content'),
+		forgotContent = $('.js-forgot-content'),
+		backBtn = $('.js-btn-back');
+
+	forgotLink.on('click', function(e){
+		e.preventDefault();
+		loginContent.fadeOut(300, function(){
+			forgotContent.fadeIn(300);
+		})
+	});
+	backBtn.on('click', function(e){
+		e.preventDefault();
+		forgotContent.fadeOut(300, function(){
+			loginContent.fadeIn(300);
+		})
+	})
+}
